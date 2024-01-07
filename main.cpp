bdfdf391081417806dfa78865459042234d1f64c
sssnowyue <sssnowyue@163.com>
2024-01-07 21:01:46 +0000

    I/O multiplexing Epoll

--- main.cpp
+++ main.cpp
@@ -8,8 +8,19 @@
 #include <unistd.h>
 #include <string.h>
 #include "util.h"
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define MAX_EVENTS 1024
+#define READ_BUFFER 1024
 
 const unsigned short port = 8080;
+
+void setnonblocking(int sockfd) {
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+}
 
 int main() {
     // STEP1: socket()
@@ -37,29 +48,73 @@ int main() {
     // STEP3: listen();
     ret = listen(server_socket, 5);
     errif(ret == -1, "socket listen error");
+
+    // I/O multiplexing Epoll - STEP 1 : Create epoll instance
+    int epfd = epoll_create1(0);
+    errif(epfd == -1, "epoll create error");
+    // I/O multiplexing Epoll - STEP 2 : Register an event of server socket
+    struct epoll_event ev;
+    bzero(&ev, sizeof(ev));
+    ev.events = EPOLLIN | EPOLLET;
+    ev.data.fd = server_socket;
+    setnonblocking(server_socket);
+    epoll_ctl(epfd, EPOLL_CTL_ADD, server_socket, &ev);
+
+    struct epoll_event events[MAX_EVENTS];
+    bzero(&events, sizeof(events));
   
     while(true) {
-      struct sockaddr_in client_address;
-      memset(&client_address, 0, sizeof(client_address));
+      // I/O multiplexing Epoll - STEP 3 : Wait for events
+      int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+      errif(nfds == -1, "epoll wait error");
 
-      // STEP4: accept()
-      socklen_t address_len = sizeof(client_address);
-      int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &address_len);
+      for(int i = 0; i < nfds; ++i){
+        // New client connetion
+        if(events[i].data.fd == server_socket){
+          // STEP4: accept()
+          struct sockaddr_in client_address;
+          memset(&client_address, 0, sizeof(client_address));
+          socklen_t address_len = sizeof(client_address);
+          int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &address_len);
+          errif(client_socket == -1, "socket accept error");
+          printf("new client fd %d! IP: %s Port: %d\n", client_socket, inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
 
-      // STEP5: read()
-      char buffer[1024];
-      int len = read(client_socket,buffer,sizeof(buffer));
-      
-      if (len > 0) {
-        std::string data = "HTTP/1.1 200 OK\r\n\r\n<html><body>Hello World!</body></html>";
-        //STEP6: write()
-        write(client_socket, data.c_str(), data.size()); 
+          // I/O multiplexing Epoll - STEP 2 : Register an event of connected client socket
+          bzero(&ev, sizeof(ev));
+          ev.data.fd = client_socket;
+          ev.events = EPOLLIN | EPOLLET;
+          setnonblocking(client_socket);
+          epoll_ctl(epfd, EPOLL_CTL_ADD, client_socket, &ev);
+        // Ready to Read
+        }else if(events[i].events & EPOLLIN){
+          char buf[READ_BUFFER];
+          while(true){ // Read buf size data at a time until all is read.
+            // STEP5: read()
+            bzero(&buf, sizeof(buf));
+            int len = read(events[i].data.fd, buf, sizeof(buf));
+            if(len > 0){
+              printf("message from client fd %d: %s\n", events[i].data.fd, buf);
+              //STEP6: write()
+              std::string data = "HTTP/1.1 200 OK\r\n\r\n<html><body>Hello World!</body></html>";
+              write(events[i].data.fd, data.c_str(), data.size());
+            }else if(len == -1 && errno == EINTR){ // Client interrupts normally, To continues reading
+              printf("continue reading");
+              continue;
+            }else if(len == -1 && (errno == EINTR || errno == EINTR)){ // All data has been read
+              printf("finish reading once, errno: %d\n", errno);
+              break;
+            }else if(len == 0){ // Client has been disconneted
+              printf("EOF, client fd %d disconnected\n", events[i].data.fd);
+              //Close client socket (remove this socket from epoll automatically)
+              close(events[i].data.fd);
+              break;
+            }
+          }
+        }else{
+          printf("other condition to be catch in later version\n");
+        }
       }
-      
-      close(client_socket);
     }
-
     close(server_socket);
-    
     return 0;
 }
